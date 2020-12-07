6862b8bd3d18b53508e1be1c502d446b0c09a414
Monika <m.szymanska.macik@gmail.com>
2020-12-07 22:42:19 +0100

    folder models added with 2 classes. Category and Pie

--- BethanysPieShop/BethanysPieShop/Startup.cs
+++ BethanysPieShop/BethanysPieShop/Startup.cs
@@ -16,8 +16,8 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -26,14 +26,16 @@ namespace BethanysPieShop
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
